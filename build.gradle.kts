d714c719efe334cf5d6b832f411109734027a57a
kuku <kuku@kuku.me>
2023-06-14 14:46:59 +0800

    upgrade ktor to 2.3.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,10 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import java.util.Properties
 
 val springBootVersion = "2.7.11"
-val ktorVersion = "2.3.0"
+val ktorVersion = "2.3.1"
 
 plugins {
-    val kotlinVersion = "1.8.20"
+    val kotlinVersion = "1.8.22"
     kotlin("jvm") version kotlinVersion
     id("org.jetbrains.kotlin.kapt") version kotlinVersion
     `maven-publish`
@@ -15,7 +15,7 @@ plugins {
 }
 
 group = "me.kuku"
-version = "2.3.0.0"
+version = "2.3.1.0"
 
 repositories {
     maven("https://nexus.kuku.me/repository/maven-public/")
